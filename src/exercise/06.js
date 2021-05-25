ae8c4d4d67e4a5fc0458165ed457839a3e1dec71
prattvaidya <pratt.vaidya@gmail.com>
2021-05-25 15:14:49 -0700

    Forms

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,11 +4,32 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
+  const inputRef = React.useRef()
+  const btnRef = React.useRef()
+
+  const [username, setUsername] = React.useState('')
+  // const [error, setError] = React.useState(null)
   // 🐨 add a submit event handler here (`handleSubmit`).
   // 💰 Make sure to accept the `event` as an argument and call
   // `event.preventDefault()` to prevent the default behavior of form submit
   // events (which refreshes the page).
-  //
+  const handleChange = e => {
+    const {value} = e.target
+    setUsername(value.toLowerCase())
+
+    // const isValid = value === value.toLowerCase()
+    // setError(isValid ? null : 'Username must be lower case')
+    // btnRef.current.disabled = !isValid
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    onSubmitUsername(username)
+
+    // const {value} = inputRef.current
+    // onSubmitUsername(value)
+  }
+
   // 🐨 get the value from the username input (using whichever method
   // you prefer from the options mentioned in the instructions)
   // 💰 For example: event.target.elements[0].value
@@ -19,12 +40,21 @@ function UsernameForm({onSubmitUsername}) {
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="username">Username:</label>
+        {/* <input type="text" id="username" ref={inputRef} /> */}
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={handleChange}
+        />
       </div>
-      <button type="submit">Submit</button>
+      {/* <div role="alert">{error}</div> */}
+      <button type="submit" ref={btnRef}>
+        Submit
+      </button>
     </form>
   )
 }
